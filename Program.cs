063a5814cd35c2b24f8b0cb92f9cb6e6cce31c79
aliqayed <152035372+aliqayed@users.noreply.github.com>
2024-10-18 22:16:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write(" enter nummber = ");
+int number =int .Parse(Console.ReadLine());
+int SUM = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    if ((number != i)&& number % i == 0)
+    {
+        Console.WriteLine($" {number} % {i} == 0");
+        SUM += i;
+        
+    }
+    
+}
+
+if (SUM == number) {
+
+    Console.WriteLine($"number is premtive nummber is  =  {SUM}");
+
+}
+
+else {
+    Console.WriteLine($"Nummber =  {number}  Is NOT premtive "); 
+}
